54c63cae8637a1277964d42cbc9c9de74c45d2d2
PUMIN-L <puminbg@gmail.com>
2024-06-21 20:05:46 +0700

    save

---
+++ src/features/tier/components/TierContainer.jsx
@@ -0,0 +1,21 @@
+import TierCard from "./TierCard";
+
+export default function TierContainer() {
+    return (
+        <>
+            <div
+                className=" w-[60vw] m-auto mt-10 p-6">
+                <h2
+                    className=" font-bold text-3xl"
+                >
+                    Select your reward
+                </h2>
+                <h3
+                    className=" text-xl mb-5"
+                >Select an option below
+                </h3>
+                <TierCard />
+            </div>
+        </>
+    )
+}
