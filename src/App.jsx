21aca30f65afc61d424819af703414e182327d47
PUMIN-L <puminbg@gmail.com>
2024-06-22 01:03:30 +0700

    create-productCommentCard-and-ProductCommentContainer-finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import ProductCommentContainer from "./features/product-comment/components/ProductCommentContainer";
 
 export default function App() {
   return (
     <>
-      <ProductCommentContainer />
-      {/* <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button className="btn btn-neutral">Button</button> */}
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn btn-neutral">Button</button>
     </>
   );
 }
--- src/features/product-comment/components/ProductCommentContainer.jsx
+++ src/features/product-comment/components/ProductCommentContainer.jsx
@@ -3,8 +3,18 @@ import ProductCommentCard from "./ProductCommentCard";
 export default function ProductCommentContainer() {
     return (
         <>
-            <div>Production comment container</div>
-            <ProductCommentCard />
+            <div className="w-[70rem] bg-slate-200 m-auto p-10 mt-10">
+                <div className="flex gap-5 justify-between mb-10">
+                    <textarea className="w-full p-2"></textarea>
+                    <button className="btn bg-lime-400 text-white hover:bg-lime-500">Send</button>
+                </div>
+                <ProductCommentCard />
+                <ProductCommentCard />
+                <ProductCommentCard />
+                <ProductCommentCard />
+                <ProductCommentCard />
+            </div>
+
         </>
 
     )
