3924074c51ea0d567353d7283bd397bf6f5edc87
Suwanan Sangpisarn <suwanan.cs@hotmail.com>
2024-06-22 16:35:16 +0700

    feat:edit-image-card

--- src/components/Image-card.jsx
+++ src/components/Image-card.jsx
@@ -1,92 +1,88 @@
+const widthMap = {
+  small: "w-[300px]",
+  medium: "w-80",
+  large: "w-96",
+};
+
+const imageMap = {
+  small: "h-32",
+  medium: "h-48",
+  large: "h-60",
+};
+
+const heightMap = {
+  small: "h-48",
+  medium: "h-60",
+  large: "h-62",
+};
+
+const colorMap = {
+  primary: "hover:bg-blue-500",
+  secondary: "hover:bg-green-500",
+  danger: "hover:bg-red-500",
+};
+
+const progressBar = {
+  small: "w-32",
+  medium: "w-48",
+  large: "w-32",
+};
+
+const textMap = {
+  small: "text-xs",
+  medium: "text-sm",
+  large: "text-lg",
+};
 
 export default function ImgCard({
   imageSrc,
   productName,
   creatorName,
   daysLeft,
+  size = "",
+  buttonColor = "",
 }) {
   return (
-    <div className="block rounded-xl overflow-hidden shadow-md w-72  h-2/4">
-      <div>
-        <img
-          src={imageSrc}
-          alt={productName}
-          className="w-full h-48 object-cover bg-slate-200"
-        />
-        <div className=" h-1 w-2/4 bg-neutral-600">
-          <div className="h-1 bg-yellow-500"></div>
+    <div className="flex items-center gap-5">
+      <div
+        className={`group bg-beige-400 ${widthMap[size]} ${heightMap[size]} hover:h-auto shadow-md rounded-md overflow-hidden`}
+      >
+        <div className="h-40 relative overflow-hidden">
+          <img
+            src="https://static.thairath.co.th/media/B6FtNKtgSqRqbnNsbKEfQbPGELW2YjCcDQUpDilBzR4jVwhRbzfUfbngdYegm1bTfTXjc.webp"
+            className={`absolute top-0 left-0 ${imageMap[size]} object-cover transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:duration-[1s]`}
+          />
         </div>
-      </div>
-
-      <div className="flex flex-row justify-between">
-        <div className="flex gap-3 p-4 ">
-          <div>
+        {/* progress bar */}
+        <div className="h-1 bg-neutral-300">
+          <div className={`h-1 ${progressBar[size]} bg-yellow-500`}></div>
+        </div>
+        {/* product detail */}
+        <div className="flex flex-row ml-4 gap-2 overflow-visible ">
+          <div name="avatar">
             <img
               src="https://mdbcdn.b-cdn.net/img/new/avatars/1.webp"
-              class="w-6 rounded-full shadow-lg"
+              className="w-48 rounded-full shadow-lg mt-2"
               alt="Avatar"
             />
           </div>
-          <div>
-            <h3 className="text-xl font-bold">
-              <p>Product Name: </p>
-              {productName}
-            </h3>
-            <p className="text-gray-600">creator name{creatorName}</p>
-            <div className="flex items-center mt-2 gap-2 text-">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-4"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                />
-              </svg>
-
-              <span className="mr-2 text-xs text-gray-500 font-medium">
-                7 days left {daysLeft}
-              </span>
+          <div className="m-1">
+            <p className="text-lg tex">Product name{productName}</p>
+            <p className="text-gray-600 text-xs">Creator: {creatorName}</p>
+            <span className="mr-2 text-xs text-gray-500 font-medium">
+              {daysLeft} days left
+            </span>
+            <div className="mt-2 opacity-0 group-hover:opacity-100 duration-[1s] -translate-y-6 group-hover:translate-y-0">
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book.
+              </p>
             </div>
           </div>
-        </div>
-
-        <div>
-        <button className="">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className={`size-6 mt-5`}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z"
-            />
-          </svg>
-          </button>
         </div>
       </div>
     </div>
   );
 }
-
-/* Property 1=Default */
-
-// position: absolute;
-// width: 288px;
-// height: 226px;
-// left: 20px;
-// top: 20px;
-
-// background: #FFFFFF;
-// box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-// border-radius: 10px;
