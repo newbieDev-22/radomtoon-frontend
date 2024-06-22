43ad5bcd2b99a000152d3236485cc933fc0b1614
tharalit <tharalit.b@gmail.com>
2024-06-23 00:57:39 +0700

    feature/creator-register-model finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import CreatorRegisterForm from "./components/CreatorRegisterForm";
+
 export default function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button className="btn btn-neutral">Button</button>
+      {/* <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn btn-neutral">Button</button> */}
+      <CreatorRegisterForm />
     </>
   );
 }
