10015dd9f4b74894426e0daa105351c8b98b169e
Suwanan Sangpisarn <suwanan.cs@hotmail.com>
2024-06-22 16:40:32 +0700

    feat:edit-homepage-layout

--- src/layouts/Homepage-layout.jsx
+++ src/layouts/Homepage-layout.jsx
@@ -1,10 +1,11 @@
 import ImgCard from "../components/Image-card";
+import MainImageCard from "../components/Main-Image-card";
 
 export default function HomePageLayOut(){
     return(
         <div className="flex justify-center mt-20">
             <div>
-                <ImgCard size="large" />
+                <MainImageCard size="large" />
             </div>
             <div className="grid grid-cols-2 gap-4 ml-4">
                 <ImgCard size="medium" />
