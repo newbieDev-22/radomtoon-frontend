36398acdd927ebe78160ab98329a3fbdfbae8e8a
tharalit <tharalit.b@gmail.com>
2024-06-23 01:32:45 +0700

    feature/kogin-page finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import LoginPage from "./pages/LoginPage";
+
 export default function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button className="btn btn-neutral">Button</button>
+      {/* <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn btn-neutral">Button</button> */}
+      <LoginPage />
     </>
   );
 }
