32f8ea054cfd6d4b85021f7b6088c1e3a22a0fcc
drivezz22 <puwanut.pansailom@gmail.com>
2024-06-24 16:39:15 +0700

    fix : fix image card

---
+++ src/constants/index.js
@@ -0,0 +1,35 @@
+export const widthMap = {
+  small: "w-[300px]",
+  medium: "w-80",
+  large: "w-96",
+};
+
+export const imageMap = {
+  small: "h-32",
+  medium: "h-48",
+  large: "h-60",
+};
+
+export const heightMap = {
+  small: "h-48",
+  medium: "h-60",
+  large: "h-64",
+};
+
+export const colorMap = {
+  primary: "hover:bg-blue-500",
+  secondary: "hover:bg-green-500",
+  danger: "hover:bg-red-500",
+};
+
+export const progressBar = {
+  small: "w-32",
+  medium: "w-48",
+  large: "w-32",
+};
+
+export const textMap = {
+  small: "text-xs",
+  medium: "text-sm",
+  large: "text-lg",
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,66 @@
-import Editor from "../components/EditorComponent/Editor";
+import ImgCard from "../components/ImageCard";
+
+const mockImage =
+  "https://static.thairath.co.th/media/B6FtNKtgSqRqbnNsbKEfQbPGELW2YjCcDQUpDilBzR4jVwhRbzfUfbngdYegm1bTfTXjc.webp";
+const mockAvatar = "https://mdbcdn.b-cdn.net/img/new/avatars/1.webp";
+const mockProjectName =
+  "ascsacascasssssssssssssssssssssssssssssssssssssssssssssssssssssssc";
+const mockCreatorName = "safassafsafsfa";
+const daysLeft = 23;
 
 export default function Home() {
   return (
     <div>
-      <Editor />
+      <div className="flex justify-center mt-20">
+        <div>
+          <ImgCard
+            widthSize="large"
+            heightSize="large"
+            progressSize="large"
+            imageSize="large"
+            imageSrc={mockImage}
+            productName={mockProjectName}
+            creatorName={mockCreatorName}
+            daysLeft={daysLeft}
+            avatarImage={mockAvatar}
+            mainCard={true}
+          />
+        </div>
+        <div className="grid grid-cols-2 gap-4 ml-4">
+          <ImgCard
+            size="medium"
+            imageSrc={mockImage}
+            productName={mockProjectName}
+            creatorName={mockCreatorName}
+            daysLeft={daysLeft}
+            avatarImage={mockAvatar}
+          />
+          <ImgCard
+            size="medium"
+            imageSrc={mockImage}
+            productName={mockProjectName}
+            creatorName={mockCreatorName}
+            daysLeft={daysLeft}
+            avatarImage={mockAvatar}
+          />
+          <ImgCard
+            size="medium"
+            imageSrc={mockImage}
+            productName={mockProjectName}
+            creatorName={mockCreatorName}
+            daysLeft={daysLeft}
+            avatarImage={mockAvatar}
+          />
+          <ImgCard
+            size="medium"
+            imageSrc={mockImage}
+            productName={mockProjectName}
+            creatorName={mockCreatorName}
+            daysLeft={daysLeft}
+            avatarImage={mockAvatar}
+          />
+        </div>
+      </div>
     </div>
   );
 }
