337dd92d50ce20df4fc738a8724086cf3c442dc6
drivezz22 <puwanut.pansailom@gmail.com>
2024-06-24 14:26:43 +0700

    fix : css editor

--- src/layouts/CampaignSection.jsx
+++ src/layouts/CampaignSection.jsx
@@ -1,17 +1,17 @@
-import CampaignSectionItem from "./CampaignSectionItem"
+import CampaignSectionItem from "./CampaignSectionItem";
 
 const sections = [
-  {id:1, tab:'Story'},
-  {id:2, tab:'Reward'},
-  {id:3, tab:'Forum'},
-]
+  { id: 1, tab: "Story" },
+  { id: 2, tab: "Reward" },
+  { id: 3, tab: "Forum" },
+];
 
 export default function CampaignSection() {
   return (
-    <section className='h-[10vh] my-[10vh] flex items-center gap-20 text-white pl-[10vw] bg-radomtoon-dark'>
-        {sections.map(sec => 
-          <CampaignSectionItem name={sec.name} tab={sec.tab} />
-        )}
+    <section className="h-[10vh] my-[10vh] flex items-center gap-20 text-white pl-[10vw] bg-radomtoon-dark">
+      {sections.map((sec) => (
+        <CampaignSectionItem key={sec.id} name={sec.name} tab={sec.tab} />
+      ))}
     </section>
-  )
+  );
 }
