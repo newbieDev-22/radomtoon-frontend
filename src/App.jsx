33eb96c2ff5de87d95dec3ccdf86d0b6bfef4dc6
tharalit <tharalit.b@gmail.com>
2024-06-25 14:12:32 +0700

    project-approval-management-table

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import Router from "./routes";
 import { Suspense } from "react";
-
-import Table from "./components/Table";
-
 export default function App() {
   return (
-    // <Suspense fallback={"Loading..."}>
-    //   <Router />
-    // </Suspense>
-    <Table />
+    <Suspense fallback={"Loading..."}>
+      <Router />
+    </Suspense>
   );
 }
--- src/components/ProjectApprovalManagementTable.jsx
+++ src/components/ProjectApprovalManagementTable.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import GridTable from "./GridTable";
 
 export default function ProjectApprovalManagementTable() {
-  return <div>ProjectApprovalManagementTable</div>;
+  const columns = ["Creator name", "Project name", "Status"];
+  const data = [
+    {
+      name: "John Doe",
+      form: "Project 1",
+      status: "success",
+    },
+    {
+      name: "John Doe",
+      form: "Project 2",
+      status: "pending",
+    },
+    {
+      name: "Dean Thomas",
+      form: "Project 3",
+      status: "failed",
+    },
+  ];
+  return (
+    <GridTable
+      columns={columns}
+      data={data}
+      title="Project Approval Management"
+    />
+  );
 }
