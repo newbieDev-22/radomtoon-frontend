47ad715f66369872d280fa0a1d916afa33e1b933
PUMIN-L <puminbg@gmail.com>
2024-06-25 18:10:09 +0700

    create-payment-form

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -1,3 +1,11 @@
+import PaymentFeature from "../features/payment/components/PaymentFeature";
+import TierCard from "../features/tier/components/TierCard";
+
 export default function Payment() {
-  return <div>Payment</div>;
+  return (
+
+    <div className="flex justify-between p-32 px-48">
+      <PaymentFeature />
+    </div>
+  )
 }
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,5 +1,7 @@
 import { lazy } from "react";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+// import Payment from "../pages/Payment";
+// import Completion from "../features/payment/components/Completion";
 
 const Header = lazy(() => import('../layouts/Header'))
 const Footer = lazy(() => import('../layouts/Footer'))
@@ -13,31 +15,33 @@ const Payment = lazy(() => import('../pages/Payment'))
 const SupporterPanel = lazy(() => import('../pages/SupporterPanel'))
 const CreatorPanel = lazy(() => import('../pages/CreatorPanel'))
 const AdminPanel = lazy(() => import('../pages/AdminPanel'))
+const Completion = lazy(() => import("../features/payment/components/Completion"))
 
-const router = createBrowserRouter([ {
+const router = createBrowserRouter([{
   path: '/',
   element:
-  <>
-    <Header />
-    <div className="min-h-[80vh]"><Outlet /></div>
-    <Footer />
-  </>,
+    <>
+      <Header />
+      <div className="min-h-[80vh]"><Outlet /></div>
+      <Footer />
+    </>,
   children: [
-    { path: '/landing', element: <Landing />},
-    { path: '/', element: <Home />},
-    { path: '/login', element: <LogIn />},
-    { path: '/register', element: <CreatorRegister />},
+    { path: '/landing', element: <Landing /> },
+    { path: '/', element: <Home /> },
+    { path: '/login', element: <LogIn /> },
+    { path: '/register', element: <CreatorRegister /> },
 
-    { path: '/campaign/:productId', element: <Campaign />},
-    { path: '/campaign/:productId/payment', element: <Payment />},
-    { path: '/campaign/:productId/tiers', element: <Tier />},
-    
-    { path: '/supporter/:supporterId', element: <SupporterPanel />},
-    { path: '/creator/:creatorId', element: <CreatorPanel />},
-    { path: '/admin-panel', element: <AdminPanel />},
-    
+    { path: '/campaign/:productId', element: <Campaign /> },
+    { path: '/campaign/:productId/payment', element: <Payment /> },
+    { path: '/campaign/:productId/payment/completion', element: <Completion /> },
+    { path: '/campaign/:productId/tiers', element: <Tier /> },
+
+    { path: '/supporter/:supporterId', element: <SupporterPanel /> },
+    { path: '/creator/:creatorId', element: <CreatorPanel /> },
+    { path: '/admin-panel', element: <AdminPanel /> },
+
   ]
-} ])
+}])
 
 export default function Router() {
   return <RouterProvider router={router} />
