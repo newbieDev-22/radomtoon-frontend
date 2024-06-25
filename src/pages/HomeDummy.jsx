36a9a78dfdd05ae53d13f8ab079e8a5bce7fecb2
BbreeSwn <suwanan.cs@hotmail.com>
2024-06-25 16:57:41 +0700

    fix:category component

--- src/pages/HomeDummy.jsx
+++ src/pages/HomeDummy.jsx
@@ -1,32 +1,9 @@
-import ImgCard from "../components/ImageCard";
-
-const mockImage = "https://static.thairath.co.th/media/B6FtNKtgSqRqbnNsbKEfQbPGELW2YjCcDQUpDilBzR4jVwhRbzfUfbngdYegm1bTfTXjc.webp";
-const mockAvatar = "https://mdbcdn.b-cdn.net/img/new/avatars/1.webp";
-const mockProjectName = "ascsacascasssssssssssssss ssssssssssssssssssssssssssssssssssssssssc";
-const mockCreatorName = "safassafsafsfa";
-const daysLeft = 23;
-
-const loopCard = 16;
+import CategoryContainer from "../components/CategoryContainer";
 
 export default function HomeDummy() {
   return (
     <div>
-      <h1 className="flex justify-center text-5xl text-semibold mt-10">Title</h1>
-      <div className="flex justify-center mt-10 mb-40">
-        <div className="grid grid-cols-3 gap-10 ml-4 ">
-          {Array.from({ length: loopCard }).map((_, index) => (
-            <ImgCard
-              key={index}
-              size="medium"
-              imageSrc={mockImage}
-              productName={mockProjectName}
-              creatorName={mockCreatorName}
-              daysLeft={daysLeft}
-              avatarImage={mockAvatar}
-            />
-          ))}
-        </div>
-      </div>
+      <CategoryContainer />
     </div>
   );
 }
