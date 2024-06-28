f8ae5c815e479c87ba7ee2106ed100101acab074
drivezz22 <puwanut.pansailom@gmail.com>
2024-06-28 09:53:10 +0700

    fix : fix modal

--- src/pages/HomeDummy.jsx
+++ src/pages/HomeDummy.jsx
@@ -1,5 +1,11 @@
+import Modal from "../components/Modal";
 import CategoryContainer from "../features/home-filter/components/CategoryContainer";
 
 export default function HomeDummy() {
-  return <CategoryContainer />;
+  // return <CategoryContainer />;
+  return (
+    <Modal width={50} open={true}>
+      test
+    </Modal>
+  );
 }
