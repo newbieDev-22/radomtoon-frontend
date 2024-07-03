5713ec87508400d65e5e2ea986595be977226393
tharalit <tharalit.b@gmail.com>
2024-07-03 18:29:57 +0700

    add motion creator register page

--- src/pages/CreatorRegister.jsx
+++ src/pages/CreatorRegister.jsx
@@ -40,12 +40,12 @@ const initialInputError = {
 const slideRegisterForm = {
   initial: { opacity: 0, x: -20 },
   animate: { opacity: 1, x: 0 },
-  transition: { type: "spring", duration: 0.6, ease: "easeOut" },
+  transition: { type: "spring", duration: 0.8, ease: "easeOut" },
 };
 const slideUploadId = {
   initial: { opacity: 0, x: 20 },
   animate: { opacity: 1, x: 0 },
-  transition: { type: "spring", duration: 0.6, ease: "easeOut" },
+  transition: { type: "spring", duration: 0.8, ease: "easeOut" },
 };
 
 const popupImg = {
@@ -160,178 +160,178 @@ export default function CreatorRegister() {
       <div className="min-w-screen min-h-screen">
         <div className="relative h-screen w-full">
           <img
-            src="https://img.freepik.com/free-photo/business-people-are-brainstorming_53876-137640.jpg?t=st=1719998012~exp=1720001612~hmac=57706cfbab3adaf7d9e782f3488b605a361057959f0c33ba7d32b9e033af5c84&w=1480"
+            src="https://images.unsplash.com/photo-1483546363825-7ebf25fb7513?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt=""
             className="h-full w-full object-cover"
           />
         </div>
-        <div className="absolute inset-0 flex justify-between items-center rounded-lg w-full px-8">
+        <div className="absolute inset-0 flex justify-center items-center rounded-lg w-full">
           <motion.div
             {...slideRegisterForm}
-            className="shadow-lg bg-slate-100 bg-opacity-70 backdrop-blur-md px-20 rounded-xl flex w-[800px] h-[700px] flex-col justify-center"
+            className="shadow-lg bg-slate-100 bg-opacity-70 backdrop-blur-md px-20 rounded-xl w-[80vw] h-[700px] gap-14 flex justify-between"
           >
-            <h1 className="text-4xl text-center mb-2 font-bold text-radomtoon-dark">
-              Bring your imagination to life
-            </h1>
-            <h2 className="text-center text-gray-600 mb-8">
-              A hub for visionaries to explore cutting-edge technology early.
-            </h2>
-            <form onSubmit={handleSubmit}>
-              <div>
-                <div className="grid grid-col-2 gap-x-4 gap-y-2">
-                  <div>
-                    <Input
-                      type="text"
-                      placeholder="First name"
-                      value={input.firstName}
-                      name="firstName"
-                      onChange={handleChangeInput}
-                      error={inputError.firstName}
-                    />
-                  </div>
-                  <div>
-                    <Input
-                      type="text"
-                      placeholder="Last name"
-                      value={input.lastName}
-                      name="lastName"
-                      onChange={handleChangeInput}
-                      error={inputError.lastName}
-                    />
-                  </div>
-                  <div className="col-span-2">
-                    <Input
-                      type="text"
-                      placeholder="Email"
-                      value={input.email}
-                      name="email"
-                      onChange={handleChangeInput}
-                      error={inputError.email}
-                    />
-                  </div>
-                  <div className="col-span-2">
-                    <Input
-                      type="text"
-                      placeholder="Phone number"
-                      value={input.phone}
-                      name="phone"
-                      onChange={handleChangeInput}
-                      error={inputError.phone}
-                    />
-                  </div>
-                  <div className="col-span-2 grid grid-cols-2 gap-4">
+            <div className="flex flex-col justify-center w-1/2">
+              <h1 className="text-4xl text-center mb-2 font-bold text-radomtoon-dark">
+                Bring your imagination to life
+              </h1>
+              <h2 className="text-center text-gray-600 mb-8">
+                A hub for visionaries to explore cutting-edge technology early.
+              </h2>
+              <form className="" onSubmit={handleSubmit}>
+                <div>
+                  <div className="grid grid-col-2 gap-x-2 gap-y-2">
                     <div>
                       <Input
-                        type="password"
-                        placeholder="Password"
-                        value={input.password}
-                        name="password"
+                        type="text"
+                        placeholder="First name"
+                        value={input.firstName}
+                        name="firstName"
                         onChange={handleChangeInput}
-                        error={inputError.password}
+                        error={inputError.firstName}
                       />
                     </div>
                     <div>
                       <Input
-                        type="password"
-                        placeholder="Confirm password"
-                        value={input.confirmPassword}
-                        name="confirmPassword"
+                        type="text"
+                        placeholder="Last name"
+                        value={input.lastName}
+                        name="lastName"
                         onChange={handleChangeInput}
-                        error={inputError.confirmPassword}
+                        error={inputError.lastName}
+                      />
+                    </div>
+                    <div className="col-span-2">
+                      <Input
+                        type="text"
+                        placeholder="Email"
+                        value={input.email}
+                        name="email"
+                        onChange={handleChangeInput}
+                        error={inputError.email}
+                      />
+                    </div>
+                    <div className="col-span-2">
+                      <Input
+                        type="text"
+                        placeholder="Phone number"
+                        value={input.phone}
+                        name="phone"
+                        onChange={handleChangeInput}
+                        error={inputError.phone}
+                      />
+                    </div>
+                    <div className="col-span-2 grid grid-cols-2 gap-4">
+                      <div>
+                        <Input
+                          type="password"
+                          placeholder="Password"
+                          value={input.password}
+                          name="password"
+                          onChange={handleChangeInput}
+                          error={inputError.password}
+                        />
+                      </div>
+                      <div>
+                        <Input
+                          type="password"
+                          placeholder="Confirm password"
+                          value={input.confirmPassword}
+                          name="confirmPassword"
+                          onChange={handleChangeInput}
+                          error={inputError.confirmPassword}
+                        />
+                      </div>
+                    </div>
+
+                    <div className="col-span-2">
+                      <Input
+                        type="text"
+                        placeholder="Address"
+                        value={input.address}
+                        name="address"
+                        onChange={handleChangeInput}
+                        error={inputError.address}
+                      />
+                    </div>
+                    <div className="col-span-2 w-full h-12 ">
+                      <Dropdown
+                        data={PROVINCE_MAP.map((el) => el.name)}
+                        onChange={handleProvinceChange}
+                        title="Choose your province..."
                       />
                     </div>
                   </div>
 
-                  <div className="col-span-2">
-                    <Input
-                      type="text"
-                      placeholder="Address"
-                      value={input.address}
-                      name="address"
-                      onChange={handleChangeInput}
-                      error={inputError.address}
+                  <div className="flex items-center py-4">
+                    <input
+                      id="policy-checkbox"
+                      type="checkbox"
+                      className="mr-2"
+                      name="policy"
+                      checked={isPolicyChecked}
+                      onChange={handleCheckboxChange}
                     />
-                  </div>
-                  <div className="col-span-2 w-full h-12">
-                    <Dropdown
-                      data={PROVINCE_MAP.map((el) => el.name)}
-                      onChange={handleProvinceChange}
-                      title="Choose your province..."
-                    />
-                  </div>
-                </div>
-
-                <div className="flex items-center py-4">
-                  <input
-                    id="policy-checkbox"
-                    type="checkbox"
-                    className="mr-2"
-                    name="policy"
-                    checked={isPolicyChecked}
-                    onChange={handleCheckboxChange}
-                  />
-                  <label
-                    htmlFor="policy-checkbox"
-                    className="text-gray-600 cursor-pointer text-sm"
-                  >
-                    I have read, understand and accept the{" "}
-                    <a
-                      onClick={() => setOpenPolicyModal(true)}
-                      className="text-blue-500 hover:underline"
+                    <label
+                      htmlFor="policy-checkbox"
+                      className="text-gray-600 cursor-pointer text-sm"
                     >
-                      terms and conditions
-                    </a>
-                  </label>
-                </div>
-                {checkboxError && (
-                  <p className="text-red-500 text-sm mb-4">{checkboxError}</p>
-                )}
+                      I have read, understand and accept the{" "}
+                      <a
+                        onClick={() => setOpenPolicyModal(true)}
+                        className="text-blue-500 hover:underline"
+                      >
+                        terms and conditions
+                      </a>
+                    </label>
+                  </div>
+                  {checkboxError && (
+                    <p className="text-red-500 text-sm mb-4">{checkboxError}</p>
+                  )}
 
-                <Button
-                  width={"full"}
-                  height="14"
-                  bg="creator-saturate"
-                  color="white"
-                >
-                  Request Approve
-                </Button>
-              </div>
-            </form>
-          </motion.div>
-          <motion.div
-            {...slideUploadId}
-            className="shadow-lg bg-slate-100 bg-opacity-70 backdrop-blur-md rounded-xl w-[800px] h-[700px]  flex flex-col items-center justify-center "
-          >
-            <h3 className="text-4xl text-center font-bold text-radomtoon-dark">
-              Identity Image Preview
-            </h3>
-            {file ? (
-              <div
-                className=" flex items-center justify-center w-full p-16"
-                onClick={() => fileEl.current.click()}
-              >
-                <motion.img
-                  src={URL.createObjectURL(file)}
-                  alt="Selected"
-                  className="object-cover aspect-[16/9] w-full rounded-xl"
-                  {...popupImg}
-                />
-              </div>
-            ) : (
-              <div
-                className="cursor-pointer flex flex-col items-center justify-center m-8 border-2 border-radomtoon-bright bg-creator-saturate p-8 rounded-xl"
-                onClick={() => fileEl.current.click()}
-              >
-                <img
-                  src={previewImage}
-                  alt="Selected"
-                  className="object-cover aspect-square w-1/2 opacity-50"
-                />
-                <div className="text-white text-2xl font-bold text-center">
-                  Add your identity card with your image
+                  <Button
+                    width={"full"}
+                    height="14"
+                    bg="creator-saturate"
+                    color="white"
+                  >
+                    Request Approve
+                  </Button>
                 </div>
-              </div>
-            )}
+              </form>
+            </div>
+
+            <div className="flex flex-col justify-center w-1/2">
+              <h3 className="text-4xl text-center font-bold text-radomtoon-dark inline-block">
+                Identity Image Preview
+              </h3>
+              {file ? (
+                <div
+                  className="cursor-pointer flex items-center justify-center w-full px-10"
+                  onClick={() => fileEl.current.click()}
+                >
+                  <motion.img
+                    src={URL.createObjectURL(file)}
+                    alt="Selected"
+                    className="object-cover aspect-[16/9] w-[576px] h-[352px] rounded-xl"
+                    {...popupImg}
+                  />
+                </div>
+              ) : (
+                <div
+                  className="cursor-pointer flex flex-col items-center justify-center m-8 bg-white p-8 rounded-xl shadow-lg"
+                  onClick={() => fileEl.current.click()}
+                >
+                  <img
+                    src={previewImage}
+                    alt="Selected"
+                    className=" object-cover aspect-square w-1/2 opacity-70"
+                  />
+                  <div className="opacity-80 text-2xl font-bold text-center">
+                    Add your identity card with your image
+                  </div>
+                </div>
+              )}
+            </div>
           </motion.div>
         </div>
       </div>
