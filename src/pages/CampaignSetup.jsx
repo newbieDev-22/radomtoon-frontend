e2fbd0a9aae44d4735c51cca1d53c0717ef5a347
tharalit <tharalit.b@gmail.com>
2024-07-05 17:17:39 +0700

    Re-design campaign setup

--- src/pages/CampaignSetup.jsx
+++ src/pages/CampaignSetup.jsx
@@ -10,6 +10,7 @@ import { toast } from "react-toastify";
 import validateProduct from "../validators/validate-create-project";
 import Spinner from "../components/Spinner";
 import { motion } from "framer-motion";
+import { CloudUpload } from "../icons";
 
 const initialInput = {
   productName: "",
@@ -126,21 +127,17 @@ export default function CampaignSetup() {
       />
 
       <div className="flex flex-col justify-center p-4 sm:p-6 lg:p-8 xl:p-10 2xl:p-12">
-        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-4xl font-bold mx-auto text-center mb-3">
-          CREATE YOUR PROJECT
-        </h1>
-
         <motion.div
           {...slidDownForm}
-          className="bg-white flex flex-col md:flex-row justify-center items-center mx-2 sm:mx-4 md:mx-8 lg:mx-12 xl:mx-16 2xl:mx-20 my-4 sm:my-6 lg:my-8 rounded-3xl shadow-[2px_6px_14px_1px_#00000024]"
+          className=" h-[657px] bg-white flex flex-col md:flex-row justify-center items-center mx-2 sm:mx-4 md:mx-8 lg:mx-12 xl:mx-16 2xl:mx-20 my-4 sm:my-6 lg:my-8 rounded-lg shadow-[2px_6px_14px_1px_#00000024]"
         >
-          <div className="w-full md:w-1/2 flex justify-center p-2 sm:p-4">
+          <div className="w-full md:w-5/12 flex justify-center p-2 sm:p-4 h-full bg-radomtoon-bright rounded-l-lg round ">
             {file ? (
-              <div onClick={() => fileEl.current.click()} className="w-full">
+              <div onClick={() => fileEl.current.click()} className="w-full flex justify-center items-center">
                 <motion.img
                   {...popupImg}
                   src={URL.createObjectURL(file)}
-                  className="object-cover aspect-[16/9] rounded-xl bg-red-200"
+                  className="object-cover aspect-[16/9] rounded-xl"
                   alt="product"
                 />
               </div>
@@ -150,11 +147,16 @@ export default function CampaignSetup() {
                 className="w-3/5 flex flex-col justify-center items-center p-4 sm:p-6 md:p-10 hover:rounded-xl hover:scale-105 duration-500"
                 onClick={() => fileEl.current.click()}
               >
-                <img
+                
+                <div className="w-full scale-75">
+                  <CloudUpload />
+                </div>
+
+                {/* <img
                   src="https://img.freepik.com/premium-vector/upload-file-flat-illustration_120816-71603.jpg?w=996"
                   alt="Upload"
-                />
-                <h3 className="text-lg sm:text-xl md:text-2xl lg:text-4xl font-semibold text-center">
+                /> */}
+                <h3 className="text-white text-lg sm:text-xl md:text-2xl lg:text-4xl font-semibold text-center">
                   Add product image
                 </h3>
               </div>
@@ -163,8 +165,12 @@ export default function CampaignSetup() {
 
           <form
             onSubmit={handleSubmitForm}
-            className="flex flex-col gap-2 bg-[#e7f5fc] rounded-xl md:rounded-none md:rounded-tr-xl md:rounded-br-xl w-full md:w-1/2 px-4 sm:px-6 md:px-8 lg:px-12 xl:px-16 py-4 sm:py-6 md:py-8"
+            className=" flex flex-col gap-2 bg-white rounded-xl md:rounded-none md:rounded-tr-xl md:rounded-br-xl w-full md:w-7/12 px-4 sm:px-6 md:px-8 lg:px-12 xl:px-16 py-4 sm:py-6 md:py-8"
           >
+            <h1 className="flex justify-center text-radomtoon-bright text-2xl sm:text-3xl md:text-4xl lg:text-4xl font-bold mb-3">
+              PROJECT SETUP
+            </h1>
+
             <label className="form-control w-full">
               <div className="label">
                 <span className="text-gray-500 font-semibold text-sm sm:text-base md:text-lg">
@@ -179,81 +185,83 @@ export default function CampaignSetup() {
                 error={inputError.productName}
               />
             </label>
-
-            <label className="form-control w-full">
-              <div className="label">
-                <span className="text-gray-500 font-semibold text-sm sm:text-base md:text-lg">
-                  Goal (THB)
-                </span>
+            <div className="hidden sm:flex gap-6 h-full">
+              <div className="flex flex-col w-2/6">
+                <label className="form-control w-full">
+                  <div className="label">
+                    <span className="text-gray-500 font-semibold text-sm sm:text-base md:text-lg">
+                      Goal (THB)
+                    </span>
+                  </div>
+                  <Input
+                    placeholder="Goals"
+                    name="goal"
+                    type="number"
+                    value={input.goal}
+                    onChange={handleInputChange}
+                    error={inputError.goal}
+                  />
+                </label>
+                <label className="form-control w-full">
+                  <div className="label">
+                    <span className="text-gray-500 font-semibold text-sm sm:text-base md:text-lg">
+                      Deadline
+                    </span>
+                  </div>
+                  <input
+                    type="date"
+                    value={dayjs(input.deadline).format("YYYY-MM-DD")}
+                    name="deadline"
+                    onChange={handleInputChange}
+                    className={`bg-gray-50 border ${
+                      inputError.deadline ? "border-red-500" : "border-gray-300"
+                    } text-gray-900 text-sm sm:text-base md:text-lg rounded-lg block p-2.5 w-full`}
+                  />
+                  {inputError.deadline && (
+                    <small className="text-red-500 font-semibold">
+                      {inputError.deadline}
+                    </small>
+                  )}
+                </label>
+                <label className="form-control ">
+                  <div className="label">
+                    <span className="text-gray-500 font-semibold text-sm sm:text-base md:text-lg">
+                      Category
+                    </span>
+                  </div>
+                  <div className="h-10">
+                    <Dropdown
+                      data={CATEGORIES_TYPE.map((el) => el.name)}
+                      onChange={handleCategoryChange}
+                      title="Choose your category..."
+                    />
+                  </div>
+                </label>
               </div>
-              <Input
-                placeholder="Goals"
-                name="goal"
-                type="number"
-                value={input.goal}
-                onChange={handleInputChange}
-                error={inputError.goal}
-              />
-            </label>
-
-            <div className="flex flex-col md:flex-row gap-2 md:gap-4">
-              <label className="form-control w-full">
+              <label className="form-control w-4/6">
                 <div className="label">
                   <span className="text-gray-500 font-semibold text-sm sm:text-base md:text-lg">
-                    Deadline
+                    Summary Detail
                   </span>
                 </div>
-                <input
-                  type="date"
-                  value={dayjs(input.deadline).format("YYYY-MM-DD")}
-                  name="deadline"
+                <textarea
+                  placeholder="Please fill summary detail"
+                  name="summaryDetail"
+                  value={input.summaryDetail}
                   onChange={handleInputChange}
-                  className={`bg-gray-50 border ${
-                    inputError.deadline ? "border-red-500" : "border-gray-300"
-                  } text-gray-900 text-sm sm:text-base md:text-lg rounded-lg block p-2.5 w-full`}
-                />
-                {inputError.deadline && (
+                  className={`placeholder-gray-400 indent-1 h-full border ${
+                    inputError.summaryDetail
+                      ? "border-red-500"
+                      : "border-gray-300"
+                  } text-gray-500 text-sm sm:text-base rounded-lg block p-2.5 w-full`}
+                ></textarea>
+                {inputError.summaryDetail && (
                   <small className="text-red-500 font-semibold">
-                    {inputError.deadline}
+                    {inputError.summaryDetail}
                   </small>
                 )}
               </label>
-              <label className="form-control w-full">
-                <div className="label">
-                  <span className="text-gray-500 font-semibold text-sm sm:text-base md:text-lg">
-                    Category
-                  </span>
-                </div>
-                <Dropdown
-                  data={CATEGORIES_TYPE.map((el) => el.name)}
-                  onChange={handleCategoryChange}
-                  title="Choose your category..."
-                />
-              </label>
             </div>
-
-            <label className="form-control w-full">
-              <div className="label">
-                <span className="text-gray-500 font-semibold text-sm sm:text-base md:text-lg">
-                  Summary Detail
-                </span>
-              </div>
-              <textarea
-                placeholder="Please fill summary detail"
-                name="summaryDetail"
-                value={input.summaryDetail}
-                onChange={handleInputChange}
-                className={`placeholder-gray-400 indent-1 min-h-24 max-h-24 border ${
-                  inputError.summaryDetail ? "border-red-500" : "border-gray-300"
-                } text-gray-500 text-sm sm:text-base rounded-lg block p-2.5 w-full`}
-              ></textarea>
-              {inputError.summaryDetail && (
-                <small className="text-red-500 font-semibold">
-                  {inputError.summaryDetail}
-                </small>
-              )}
-            </label>
-
             <label className="form-control w-full">
               <div className="label">
                 <span className="text-gray-500 font-semibold text-sm sm:text-base md:text-lg">
@@ -269,8 +277,8 @@ export default function CampaignSetup() {
               />
             </label>
 
-            <div className="py-4">
-              <Button bg="creator-saturate" color="white" width="full">
+            <div className="py-4 flex justify-center">
+              <Button bg="radomtoon-bright" color="white" width="60">
                 Save
               </Button>
             </div>
