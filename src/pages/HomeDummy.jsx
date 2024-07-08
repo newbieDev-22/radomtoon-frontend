eb1806376e6b2c66d31dd4888f2909285de93a3b
PuwanutP <104191234+newbieDev-22@users.noreply.github.com>
2024-07-08 16:53:55 +0700

    Update HomeDummy.jsx

--- src/pages/HomeDummy.jsx
+++ src/pages/HomeDummy.jsx
@@ -1,6 +1,5 @@
 import AddMilestone from "../features/product-milestone/components/AddMilestone";
-import NewAddMilestone from "../tmp/NewAddMilestone";
 
 export default function HommyDummy() {
-  return <NewAddMilestone />;
+  return <AddMilestone />;
 }
