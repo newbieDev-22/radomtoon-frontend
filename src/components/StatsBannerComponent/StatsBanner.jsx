2506e6e6e366aa6c27a7a83477430b5aebab9e9c
BbreeSwn <suwanan.cs@hotmail.com>
2024-07-09 14:28:03 +0700

    fixed: add hero banner

--- src/components/StatsBannerComponent/StatsBanner.jsx
+++ src/components/StatsBannerComponent/StatsBanner.jsx
@@ -4,12 +4,12 @@ export default function StatsBanner({ data, bg }) {
 
   return (
     <div className={`h-[30vh] md:h-[40vh] w-screen flex items-center justify-center relative`}>
-      <div className="h-[10vh] md:h-[15vh] w-[80vw] px-3 flex items-center text-center justify-evenly bg-white overflow-auto scroll-hidden shadow-md">
+      <div className="h-[10vh] md:h-[15vh] w-[80vw] px-3 flex items-center text-center justify-evenly bg-white overflow-auto scroll-hidden shadow-md rounded-xl">
         {data.map((item, index, arr) => (
           <StatsItem key={item.title} amount={item.amount} title={item.title} currency={item.currency} isLast={index === arr.length - 1} />)
         )}
       </div>
-      <img src={bg} className='absolute w-full h-full -z-10 object-cover bg-creator-normal' alt="" />
+      <img src={bg} className='absolute w-full h-[60vh] bottom-1 -z-10 object-cover bg-creator-normal' alt="" />
     </div>
   )
 }
