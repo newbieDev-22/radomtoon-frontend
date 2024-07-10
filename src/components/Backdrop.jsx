36d622ab408c0afc2528de578cbc779b3d22a8a6
drivezz22 <puwanut.pansailom@gmail.com>
2024-07-11 02:40:41 +0700

    fix : fix modal

---
+++ src/components/Backdrop.jsx
@@ -0,0 +1,14 @@
+import { motion } from "framer-motion";
+
+export default function Backdrop({ children }) {
+  return (
+    <motion.div
+      className="backdrop"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      {children}
+    </motion.div>
+  );
+}
--- src/pages/PaymentPage.jsx
+++ src/pages/PaymentPage.jsx
@@ -1,14 +1,15 @@
 import { useEffect } from "react";
 import PaymentFeature from "../features/payment/components/PaymentFeature";
 import TierSummaryCard from "../features/payment/components/TierSummaryCard";
-
+import PaymentGIF from "../assets/images/payment.json";
+import Lottie from "lottie-react";
 export default function PaymentPage() {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
   return (
-    <div className="flex py-10 px-20 justify-center gap-20 bg-creator-normal">
+    <div className="flex py-14 px-20 justify-center gap-20 bg-creator-normal">
       <div className="flex flex-row  p-3 rounded-lg shadow-lg bg-white">
         <div className="flex items-center px-4 ">
           <PaymentFeature />
@@ -16,6 +17,9 @@ export default function PaymentPage() {
         <div className="flex items-center px-4">
           <TierSummaryCard />
         </div>
+      </div>
+      <div>
+        <Lottie animationData={PaymentGIF} loop={true} />
       </div>
     </div>
   );
