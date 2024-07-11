2fc330a34e0204debcd576a8dfc7ecc3207b6f8f
tharalit <tharalit.b@gmail.com>
2024-07-11 11:54:58 +0700

    edit status title MilestoneForm

--- src/features/admin/components/MilestoneForm.jsx
+++ src/features/admin/components/MilestoneForm.jsx
@@ -1,12 +1,16 @@
+import { MILESTONE_STATUS } from "../../../constants";
+
 export default function MilestoneForm({ milestoneRankId, milestoneDetail }) {
+
+  
   return (
     <div className="flex justify-center">
-      <form onSubmit={(e) => e.preventDefault()} action="">
+      <form onSubmit={(e) => e.preventDefault()}>
         <div className="flex flex-col justify-center items-center w-auto">
           <div className="flex w-full p-4 gap-4">
             <div className="flex flex-col gap-4">
-              <h1>Milestone : {milestoneRankId}</h1>
-              <p disabled className="p-2 w-full h-full">
+              <h1 >Milestone : {MILESTONE_STATUS[milestoneRankId]}</h1>
+              <p disabled className="bg-gray-100 p-4 w-full h-full rounded-lg">
                 {milestoneDetail}
               </p>
             </div>
